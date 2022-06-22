9e07a499b331167f8451f9a6b8e5327d17d544f6
Matheus <matheus.augustoreal@gmail.com>
2022-06-22 20:29:29 -0300

    Create json server

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FormEvent, useState } from "react"
+import { SearchResults } from "../components/SearchResults"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function Home() {
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState([])
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault()
+
+    if (!search.trim()) {
+      return
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json()
+
+    setResults(data)
+  }
+
+  return (
+    <div>
+      <h1>Search</h1>      
+
+      <form onSubmit={handleSearch}>
+        <input 
+          type="text" 
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+          <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
+  )
+}
